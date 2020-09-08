85be2b09a065e9ea495e39448c3a4031cb966322
Bridgit Arika <bridgitmob@gmail.com>
2020-09-08 15:36:29 -0500

    more on mod2 exercises

--- mod2.py
+++ mod2.py
@@ -8,4 +8,12 @@ def list_o_matic(aput):
         return aput 
     else:
         animals.append(aput)
-        return aput       
+        return aput 
+while animals:
+    print("look at all animals: ", animals)
+    aput = input("Enter the name of an animal:")
+    if aput == "quit":
+        print("Goodbye!")
+        break 
+    else:
+        list_o_matic(aput)              
